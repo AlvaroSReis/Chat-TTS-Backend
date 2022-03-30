9a0b94ea701c94c5717de6489c775b4b5c9035b9
AlvaroSReis <57022371+AlvaroSReis@users.noreply.github.com>
2022-03-30 06:10:34 -0300

    service update

--- api.js
+++ api.js
@@ -1,43 +1,21 @@
-const client = require('./connection.js')
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
 
 
-app.use(cors());
+//app.use(cors());
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-
-app.post('/mensagem', (req, res)=> {
-    const message = req.body;
-    let insertQuery = `insert into conversas(sender, receiver, msg) 
-                       values('${message.sender}', '${message.receiver}', '${message.msg}')`
-
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Insertion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
-})
-
-app.get('/:id', (req, res)=>{
-    client.query(`Select * from conversas where sender='${req.params.id}' or receiver='${req.params.id}'`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
+io.on("connection", socket => {
+    console.log(socket.id);
+    socket.on("chat message", msg => {
+        console.log(msg);
+        io.emit("chat message", msg);
     });
-    client.end;
-})
+});
 
 
-app.listen(3300, ()=>{
-    console.log("Sever is now listening at port 3300");
-})
-
-client.connect();
+server.listen(port, () => console.log("Server running on port: " + port));
 
 
