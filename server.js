07a68ed96b1127db81901f17232cad2fefc57e5e
AlvaroSR <alvaro.reis@dcomp.ufs.br>
2022-04-08 04:24:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = 3000;
-const cors = require("cors")
-
-//app.set("ipaddr", "10.0.0.1");
+const cors = require("cors");
+const res = require("express/lib/response");
 
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('running')
+});
 
 io.on("connection", (socket) => {
     console.log("user connected")
