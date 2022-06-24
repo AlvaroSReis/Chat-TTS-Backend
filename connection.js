07ef5f534c4d514032adc035aaad2197de149459
AlvaroSR <alvaro.reis@dcomp.ufs.br>
2022-06-24 06:33:53 -0300

    dbconnect

--- connection.js
+++ connection.js
@@ -1,3 +1,5 @@
+
+/*
 const {Client} = require('pg')
 
 const client = new Client({
@@ -5,8 +7,24 @@ const client = new Client({
     user: "postgres",
     port: 5432,
     password: "admin",
-    database: "chat",
+    database: "postgres",
     schema: 'public'
+})
+
+module.exports = client
+*/
+
+
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "ec2-34-200-35-222.compute-1.amazonaws.com",
+    user: "nscxctaisxmafd",
+    port: 5432,
+    password: "b3f8eedef7c344dda8f196f935656aae773c0f65ef51a45b23f22bd58182360d",
+    database: "dd80jfhflnkbm7",
+    schema: 'public',
+    ssl: { rejectUnauthorized: false }
 })
 
 module.exports = client
--- server.js
+++ server.js
@@ -1,15 +1,106 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const port = process.env.PORT || 3000;
-const cors = require("cors");
 
+//cors
+const cors = require("cors");
 app.use(cors());
 
+//json parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+const server = require("http").createServer(app);
+const client = require('./connection.js')
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+
+
+
+//Default response for check if is up
 app.get('/', (req, res) => {
     res.send('running')
 });
+
+/*=======================================
+Gets
+=========================================*/
+
+//User login
+app.get('/login/:username&:password', (req, res)=>{
+
+    const username = String(req.params.username)
+    const senha = String(req.params.password)
+    client.query(`SELECT username, senha FROM usuario WHERE username = '${req.params.username}' AND senha = '${req.params.password}'`, (err, result)=>{
+        //console.log(`SELECT * FROM usuario WHERE username = '${req.params.username}'`)
+        if(!err){
+            res.send(result.rows);
+        }else{
+            res.send(err)
+        }
+        
+    });
+    client.end;
+})
+
+//Usuario
+
+app.get('/userinfo/:username', (req, res)=>{
+
+    const user = String(req.params.username)
+    client.query(`SELECT * FROM usuario WHERE username = '${user}';`, (err, result)=>{
+        console.log(result)
+        if(!err){
+
+            res.send(result.rows);
+        }else{
+            res.send(err)
+        }
+        
+    });
+    client.end;
+})
+
+//test
+app.get('/users', (req, res)=>{
+
+    const user = String(req.params.username)
+    client.query(`SELECT * FROM usuario`, (err, result)=>{
+        console.log(result.rows)
+        if(!err){
+
+            res.send(result.rows);
+        }else{
+            res.send(err)
+        }
+        
+    });
+    client.end;
+})
+
+
+/*=======================================
+Posts
+=========================================*/
+
+// Create new user
+app.post('/novoUsuario', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `INSERT INTO usuario(nome, username, email, senha) 
+                       VALUES('${user.nome}', '${user.username}', '${user.email}', '${user.senha}')`
+
+                       client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Sucess')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+
+/*=======================================
+Io for chat
+=========================================*/
 
 io.on("connection", (socket) => {
     console.log("user connected")
@@ -24,6 +115,7 @@ io.on("connection", (socket) => {
     });
 });
 
+client.connect();
 
 server.listen(port, () => console.log("Server running on port: " + port));
 
