0d9c2642be052e97235f607b72b70188a2e5444c
AlvaroSR <alvaro.reis@dcomp.ufs.br>
2022-07-15 10:23:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -98,6 +98,7 @@ app.get('/users', (req, res)=>{
     client.end;
 })
 
+//get friends
 app.get('/friends/:user', (req, res) => {
     const user = String(req.params.user)
     client.query(`SELECT username FROM friendship INNER JOIN usuario ON usuario1 = id OR usuario2 = id WHERE (usuario1 = (SELECT id FROM usuario WHERE username = '${user}') OR usuario2 = (SELECT id FROM usuario WHERE username ='${user}')) AND username != '${user}';`, (err, result)=> {
